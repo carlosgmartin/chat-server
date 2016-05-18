78b2f754942cfbd8148b588ad2258aed9d38bd5a
Carlos Martin <carlos.martin@columbia.edu>
2016-05-18 09:42:24 -0700

    Added socket()

--- server.c
+++ server.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include <sys/socket.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
 
 int main(int argc, char** argv)
 {
-	printf("Hello world\n");
+	int socket_descriptor = socket(AF_INET, SOCK_STREAM, 0);
+	if (socket_descriptor < 0)
+	{
+		perror("socket() error");
+		exit(errno);
+	}
 }
