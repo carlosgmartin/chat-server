6f285a045a435338fb21b95278aeab84a93e2f48
Carlos Martin <carlos.martin@columbia.edu>
2016-05-18 21:16:12 -0700

    Server now accepts and handles multiple clients

--- server.c
+++ server.c
@@ -5,6 +5,7 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/select.h>
 
 int backlog = 10;
 int recv_buffer_size = 1024;
@@ -37,34 +38,65 @@ int main(int argc, char** argv)
 		exit(errno);
 	}
 
-
-
 	char recv_buffer[recv_buffer_size];
 	char send_buffer[send_buffer_size];
+
+	fd_set active_sockets, read_sockets;
+	FD_ZERO(&active_sockets);
+	FD_SET(server_socket, &active_sockets);
+
 	while (1)
 	{
-		struct sockaddr_in client_address;
-		socklen_t client_address_length = sizeof(client_address);
-
-		int client_socket = accept(server_socket, (struct sockaddr*) &client_address, &client_address_length);
-		if (client_socket < 0)
+		read_sockets = active_sockets;
+		if (select(FD_SETSIZE, &read_sockets, NULL, NULL, NULL) < 0)
 		{
-			perror("accept() error");
+			perror("select() error");
 			exit(errno);
 		}
-		char* client_address_string = inet_ntoa(client_address.sin_addr);
-		uint16_t client_port = ntohs(client_address.sin_port);
 
-		printf("%s:%d connected\n", client_address_string, client_port);
-	
-		ssize_t bytes_received = recv(client_socket, recv_buffer, sizeof(recv_buffer) - 1, 0);
-		if (bytes_received < 0)
+		for (int socket = 0; socket < FD_SETSIZE; ++socket)
 		{
-			perror("recv() error");
-			exit(errno);
+			if (FD_ISSET(socket, &read_sockets))
+			{
+				if (socket == server_socket)
+				{
+					struct sockaddr_in client_address;
+					socklen_t client_address_length = sizeof(client_address);
+					int client_socket = accept(server_socket, (struct sockaddr*) &client_address, &client_address_length);
+					if (client_socket < 0)
+					{
+						perror("accept() error");
+						exit(errno);
+					}
+					char* client_address_string = inet_ntoa(client_address.sin_addr);
+					uint16_t client_port = ntohs(client_address.sin_port);
+					printf("%s:%d connected\n", client_address_string, client_port);
+				
+					FD_SET(client_socket, &active_sockets);
+				}
+				else
+				{
+					ssize_t bytes_received = recv(socket, recv_buffer, sizeof(recv_buffer) - 1, 0);
+					if (bytes_received < 0)
+					{
+						perror("recv() error");
+						exit(errno);
+					}
+					else if (bytes_received == 0)
+					{
+						close(socket);
+						FD_CLR(socket, &active_sockets);
+					}
+					else
+					{
+						recv_buffer[bytes_received] = '\0';
+						printf("%s", recv_buffer);
+					}
+				}
+			}
 		}
-		recv_buffer[bytes_received] = '\0';
 
+		/*
 		snprintf(send_buffer, sizeof(send_buffer), "Server says: %s", recv_buffer);
 
 		ssize_t bytes_sent = send(client_socket, send_buffer, strlen(send_buffer), 0);
@@ -75,6 +107,7 @@ int main(int argc, char** argv)
 		}
 
 		close(client_socket);
+		*/
 	}
 
 
